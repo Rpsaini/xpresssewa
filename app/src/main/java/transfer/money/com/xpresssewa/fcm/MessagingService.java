bcb889e398564183a07fbc08a6d6c7cc4aca27e9
saini35607@gmail.com <Github@607>
2021-12-15 18:39:53 +0530

    change notification icon

--- app/src/main/java/transfer/money/com/xpresssewa/fcm/MessagingService.java
+++ app/src/main/java/transfer/money/com/xpresssewa/fcm/MessagingService.java
@@ -85,14 +85,14 @@ public class MessagingService extends FirebaseMessagingService {
             mChannel.setSound(defaultSoundUri, attributes); // This is IMPORTANT
 
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-            Notification notification = mBuilder.setSmallIcon(R.mipmap.ic_launcher).setTicker(this.getResources().getString(R.string.app_name)).setWhen(0)
+            Notification notification = mBuilder.setSmallIcon(R.drawable.notification_icon).setTicker(this.getResources().getString(R.string.app_name)).setWhen(0)
                     .setAutoCancel(true)
                     .setContentTitle(title)
                     .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                     .setContentIntent(pendingIntent)
                     .setChannelId(CHANNEL_ID)
                     .setSound(defaultSoundUri)
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.drawable.notification_icon)
 
                     .setContentText(message).build();
 
@@ -101,12 +101,12 @@ public class MessagingService extends FirebaseMessagingService {
         } else {
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
                     this);
-            Notification notification = mBuilder.setSmallIcon(R.mipmap.ic_launcher).setTicker(getResources().getString(R.string.app_name)).setWhen(0)
+            Notification notification = mBuilder.setSmallIcon(R.drawable.notification_icon).setTicker(getResources().getString(R.string.app_name)).setWhen(0)
                     .setAutoCancel(true)
                     .setContentTitle(title)
                     .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                     .setContentIntent(pendingIntent)
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.drawable.notification_icon)
                     .setSound(defaultSoundUri)
                     .setContentText(message).build();
 
